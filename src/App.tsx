47ca9c42ebf49376b4e576df6aeccf0a0b321a56
diego <dmarques1508@gmail.com>
2023-12-09 15:11:42 -0300

    add prompt and slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 
-import { Github, FileVideo, Upload } from 'lucide-React'
+import { Github, FileVideo, Upload, Wand2 } from 'lucide-React'
 import { Button } from "./components/ui/button";
-import { Separator } from '@radix-ui/react-separator';
+import { Separator } from './components/ui/separator';
 import { Textarea } from './components/ui/textarea'
 import { Label } from './components/ui/label'
-import { Select, SelectValue, SelectTrigger, SelectContent, SelectItem } from '@radix-ui/react-select';
-import { Slider } from '@radix-ui/react-slider';
+import { Select, SelectValue, SelectTrigger, SelectContent, SelectItem } from './components/ui/select';
+import { Slider } from './components/ui/slider';
 
 export function App() {
   return (
@@ -14,7 +14,7 @@ export function App() {
         <h1 className="text-xl font-bold">Upload.ai</h1>
 
         <div className="flex items-center gap-3">
-          <span className="text-sm text-muted-foreground">Desenvolvido com Projeto Pessoal/ Desafio NLW
+          <span className="text-sm text-muted-foreground">Desenvolvido como Projeto Pessoal/ Desafio NLW
           </span>
 
           <Separator orientation='vertical' className='h-6'/>
@@ -66,6 +66,24 @@ export function App() {
         <Separator/>
 
         <form className='space-y-6'>
+
+
+        <div className='space-y-6'>
+          <Label>Prompt</Label>
+          <Select>
+            <SelectTrigger>
+              <SelectValue placeholder='Selecione um prompt'/>
+            </SelectTrigger>
+            <SelectContent>
+                <SelectItem value='title'>Título do Youtube</SelectItem>
+                <SelectItem value='description'>Descrição do Youtube</SelectItem>
+            </SelectContent>
+          </Select>
+          <span className='block text-xs text-muted-foreground italic'>
+            Você poderá acessar essa opção em breve
+            </span>
+            </div>
+
           <div className='space-y-6'>
           <Label>Modelo</Label>
           <Select  disabled defaultValue='gpt3.5'>
@@ -83,13 +101,24 @@ export function App() {
 
             <Separator/>
 
-            <div className='space-y-2'>
+            <div className='space-y-4'>
               <Label>Temperatura</Label>
-              <Slider/>
+              <Slider
+              min={0}
+              max={1}
+              step={0.1}
+              />
               
               <span className='block text-xs text-muted-foreground italic'>Valores mais altos tendem a deixar o resultado mais criativo e com possíveis erros.
               </span>
             </div>
+
+            <Separator/>
+
+            <Button type='submit' className='w-full'>
+              Executar
+              <Wand2 className='w-4 h-4 ml-2'/>
+            </Button>
         </form>
         </aside>
       </main>
