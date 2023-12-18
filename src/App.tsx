54629c46a6db0c1f133fbee4ab8da59ab7dd5e46
diego <dmarques1508@gmail.com>
2023-12-18 13:48:55 -0300

    Nota de roda pé

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ export function App() {
         <h1 className="text-xl font-bold">Upload.ai</h1>
 
         <div className="flex items-center gap-3">
-          <span className="text-sm text-muted-foreground">Desenvolvido como Projeto Pessoal/ Desafio NLW
+          <span className="text-sm text-muted-foreground">Desenvolvido como Projeto Pessoal
           </span>
 
           <Separator orientation='vertical' className='h-6'/>
@@ -123,8 +123,6 @@ export function App() {
         </aside>
       </main>
     </div>
-
-    
   )
 }
 
